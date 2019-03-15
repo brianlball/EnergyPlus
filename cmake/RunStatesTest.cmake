64e12713cdd5e98cac6bd912dd39e67f5955d099
Brian Ball <Brian.Ball@nrel.gov>
2019-03-15 14:57:19 -0600

    create csv's

--- cmake/RunStatesTest.cmake
+++ cmake/RunStatesTest.cmake
@@ -6,6 +6,7 @@
   execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${SOURCE_DIR}/testfiles/BriansFiles/${IDF_FILE}" "${BINARY_DIR}/${TEST_DIR}/full/in.idf" )
   execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${SOURCE_DIR}/testfiles/BriansFiles/${EPW_FILE}" "${BINARY_DIR}/${TEST_DIR}/full/in.epw" )
   execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${BINARY_DIR}/Products/Energy+.idd" "${BINARY_DIR}/${TEST_DIR}/full/Energy+.idd" )
+  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${BINARY_DIR}/Products/ReadVarsESO.exe" "${BINARY_DIR}/${TEST_DIR}/full/ReadVarsESO.exe" )
 
   # Find and execute the test executable, passing the argument of the directory to run in
   if( WIN32 )
@@ -17,7 +18,8 @@
   message( "Executing TestResetStates from ${TEST_EXE}" )
   message( "Passing run directory as ${TEST_DIR}/full" )
   execute_process(COMMAND ${ECHO_CMD} COMMAND "${TEST_EXE}" "${BINARY_DIR}/${TEST_DIR}/full")
-
+  execute_process(COMMAND ${ECHO_CMD} COMMAND "${BINARY_DIR}/${TEST_DIR}/full/ReadVarsESO.exe" WORKING_DIRECTORY "${BINARY_DIR}/${TEST_DIR}/full")
+  
   # Clean up
   #execute_process(COMMAND "${CMAKE_COMMAND}" -E remove "${BINARY_DIR}/${TEST_DIR}/full/Energy+.idd" "${BINARY_DIR}/${TEST_DIR}/full/in.epw")
 
@@ -26,7 +28,8 @@
   execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${SOURCE_DIR}/testfiles/BriansFiles/${IDF_FILE}" "${BINARY_DIR}/${TEST_DIR}/first/in.idf" )
   execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${SOURCE_DIR}/testfiles/BriansFiles/${EPW_FILE}" "${BINARY_DIR}/${TEST_DIR}/first/in.epw" )
   execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${BINARY_DIR}/Products/Energy+.idd" "${BINARY_DIR}/${TEST_DIR}/first/Energy+.idd" )
-
+  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${BINARY_DIR}/Products/ReadVarsESO.exe" "${BINARY_DIR}/${TEST_DIR}/first/ReadVarsESO.exe" )
+  
   # Find and execute the test executable, passing the argument of the directory to run in
   if( WIN32 )
     set(ECHO_CMD cmd /C echo.)
@@ -37,7 +40,8 @@
   message( "Executing TestResetStates from ${TEST_EXE}" )
   message( "Passing run directory as ${TEST_DIR}/first" )
   execute_process(COMMAND ${ECHO_CMD} COMMAND "${TEST_EXE}" "${BINARY_DIR}/${TEST_DIR}/first")
-
+  execute_process(COMMAND ${ECHO_CMD} COMMAND "${BINARY_DIR}/${TEST_DIR}/first/ReadVarsESO.exe" WORKING_DIRECTORY "${BINARY_DIR}/${TEST_DIR}/first")
+  
   # Clean up
   #execute_process(COMMAND "${CMAKE_COMMAND}" -E remove "${BINARY_DIR}/${TEST_DIR}/full/Energy+.idd" "${BINARY_DIR}/${TEST_DIR}/first/in.epw")
 
@@ -47,7 +51,8 @@
   execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${SOURCE_DIR}/testfiles/BriansFiles/${IDF_FILE}" "${BINARY_DIR}/${TEST_DIR}/last/in.idf" )
   execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${SOURCE_DIR}/testfiles/BriansFiles/${EPW_FILE}" "${BINARY_DIR}/${TEST_DIR}/last/in.epw" )
   execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${BINARY_DIR}/Products/Energy+.idd" "${BINARY_DIR}/${TEST_DIR}/last/Energy+.idd" )
-
+  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${BINARY_DIR}/Products/ReadVarsESO.exe" "${BINARY_DIR}/${TEST_DIR}/last/ReadVarsESO.exe" )
+  
   # Find and execute the test executable, passing the argument of the directory to run in
   if( WIN32 )
     set(ECHO_CMD cmd /C echo.)
@@ -58,7 +63,8 @@
   message( "Executing TestResetStates from ${TEST_EXE}" )
   message( "Passing run directory as ${TEST_DIR}/last" )
   execute_process(COMMAND ${ECHO_CMD} COMMAND "${TEST_EXE}" "${BINARY_DIR}/${TEST_DIR}/last")
-
+  execute_process(COMMAND ${ECHO_CMD} COMMAND "${BINARY_DIR}/${TEST_DIR}/last/ReadVarsESO.exe" WORKING_DIRECTORY "${BINARY_DIR}/${TEST_DIR}/last")
+  
   # Clean up
   #execute_process(COMMAND "${CMAKE_COMMAND}" -E remove "${BINARY_DIR}/${TEST_DIR}/full/Energy+.idd" "${BINARY_DIR}/${TEST_DIR}/last/in.epw")
   
