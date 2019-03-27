c8a26f3e16a6ece11f284351b843a4672ff2bfc4
Brian Ball <Brian.Ball@nrel.gov>
2019-03-27 11:54:08 -0600

    check for Small diffs or All Equal

--- cmake/RunStatesTest.cmake
+++ cmake/RunStatesTest.cmake
@@ -3,10 +3,10 @@
   #set( ENV{DDONLY} y)
 
   # Run Full Year
-#  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${SOURCE_DIR}/testfiles/BriansFiles/${IDF_FILE}" "${BINARY_DIR}/${TEST_DIR}/full/in.idf" )
-#  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${SOURCE_DIR}/testfiles/BriansFiles/${EPW_FILE}" "${BINARY_DIR}/${TEST_DIR}/full/in.epw" )
-#  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${BINARY_DIR}/Products/Energy+.idd" "${BINARY_DIR}/${TEST_DIR}/full/Energy+.idd" )
-#  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${BINARY_DIR}/Products/ReadVarsESO.exe" "${BINARY_DIR}/${TEST_DIR}/full/ReadVarsESO.exe" )
+  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${SOURCE_DIR}/testfiles/BriansFiles/${IDF_FILE}" "${BINARY_DIR}/${TEST_DIR}/full/in.idf" )
+  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${SOURCE_DIR}/testfiles/BriansFiles/${EPW_FILE}" "${BINARY_DIR}/${TEST_DIR}/full/in.epw" )
+  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${BINARY_DIR}/Products/Energy+.idd" "${BINARY_DIR}/${TEST_DIR}/full/Energy+.idd" )
+  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${BINARY_DIR}/Products/ReadVarsESO.exe" "${BINARY_DIR}/${TEST_DIR}/full/ReadVarsESO.exe" )
 
   # Find and execute the test executable, passing the argument of the directory to run in
   if( WIN32 )
@@ -15,32 +15,32 @@
     set(ECHO_CMD "echo")
   endif()
   find_program(TEST_EXE TestResetStates PATHS "${BINARY_DIR}/Products/" "${BINARY_DIR}/Products/Release/" "${BINARY_DIR}/Products/Debug/" )
-#  message( "Executing TestResetStates from ${TEST_EXE}" )
-#  message( "Passing run directory as ${TEST_DIR}/full" )
-#  execute_process(COMMAND ${ECHO_CMD} COMMAND "${TEST_EXE}" "${BINARY_DIR}/${TEST_DIR}/full")
-#  execute_process(COMMAND ${ECHO_CMD} COMMAND "${BINARY_DIR}/${TEST_DIR}/full/ReadVarsESO.exe" WORKING_DIRECTORY "${BINARY_DIR}/${TEST_DIR}/full")
+  message( "Executing TestResetStates from ${TEST_EXE}" )
+  message( "Passing run directory as ${TEST_DIR}/full" )
+  execute_process(COMMAND ${ECHO_CMD} COMMAND "${TEST_EXE}" "${BINARY_DIR}/${TEST_DIR}/full")
+  execute_process(COMMAND ${ECHO_CMD} COMMAND "${BINARY_DIR}/${TEST_DIR}/full/ReadVarsESO.exe" WORKING_DIRECTORY "${BINARY_DIR}/${TEST_DIR}/full")
   
   # Clean up
   #execute_process(COMMAND "${CMAKE_COMMAND}" -E remove "${BINARY_DIR}/${TEST_DIR}/full/Energy+.idd" "${BINARY_DIR}/${TEST_DIR}/full/in.epw")
 
   # Run First half of Year
   #TODO modify IDF to do that
-#  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${SOURCE_DIR}/testfiles/BriansFiles/${IDF_FILE}" "${BINARY_DIR}/${TEST_DIR}/first/in.idf" )
-#  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${SOURCE_DIR}/testfiles/BriansFiles/${EPW_FILE}" "${BINARY_DIR}/${TEST_DIR}/first/in.epw" )
-#  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${BINARY_DIR}/Products/Energy+.idd" "${BINARY_DIR}/${TEST_DIR}/first/Energy+.idd" )
-#  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${BINARY_DIR}/Products/ReadVarsESO.exe" "${BINARY_DIR}/${TEST_DIR}/first/ReadVarsESO.exe" )
+  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${SOURCE_DIR}/testfiles/BriansFiles/${IDF_FILE}" "${BINARY_DIR}/${TEST_DIR}/first/in.idf" )
+  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${SOURCE_DIR}/testfiles/BriansFiles/${EPW_FILE}" "${BINARY_DIR}/${TEST_DIR}/first/in.epw" )
+  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${BINARY_DIR}/Products/Energy+.idd" "${BINARY_DIR}/${TEST_DIR}/first/Energy+.idd" )
+  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${BINARY_DIR}/Products/ReadVarsESO.exe" "${BINARY_DIR}/${TEST_DIR}/first/ReadVarsESO.exe" )
   
   # Find and execute the test executable, passing the argument of the directory to run in
-  if( WIN32 )
-    set(ECHO_CMD cmd /C echo.)
-  else()
-    set(ECHO_CMD "echo")
-  endif()
+#  if( WIN32 )
+#    set(ECHO_CMD cmd /C echo.)
+#  else()
+#    set(ECHO_CMD "echo")
+#  endif()
 #  find_program(TEST_EXE TestResetStates PATHS "${BINARY_DIR}/Products/" "${BINARY_DIR}/Products/Release/" "${BINARY_DIR}/Products/Debug/" )
-#  message( "Executing TestResetStates from ${TEST_EXE}" )
-#  message( "Passing run directory as ${TEST_DIR}/first" )
-#  execute_process(COMMAND ${ECHO_CMD} COMMAND "${TEST_EXE}" "${BINARY_DIR}/${TEST_DIR}/first")
-#  execute_process(COMMAND ${ECHO_CMD} COMMAND "${BINARY_DIR}/${TEST_DIR}/first/ReadVarsESO.exe" WORKING_DIRECTORY "${BINARY_DIR}/${TEST_DIR}/first")
+  message( "Executing TestResetStates from ${TEST_EXE}" )
+  message( "Passing run directory as ${TEST_DIR}/first" )
+  execute_process(COMMAND ${ECHO_CMD} COMMAND "${TEST_EXE}" "${BINARY_DIR}/${TEST_DIR}/first")
+  execute_process(COMMAND ${ECHO_CMD} COMMAND "${BINARY_DIR}/${TEST_DIR}/first/ReadVarsESO.exe" WORKING_DIRECTORY "${BINARY_DIR}/${TEST_DIR}/first")
   
   # Clean up
   #execute_process(COMMAND "${CMAKE_COMMAND}" -E remove "${BINARY_DIR}/${TEST_DIR}/full/Energy+.idd" "${BINARY_DIR}/${TEST_DIR}/first/in.epw")
@@ -48,26 +48,26 @@
 
   # Run Last half of Year
   #TODO modify IDF to do that
-#  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${SOURCE_DIR}/testfiles/BriansFiles/${IDF_FILE}" "${BINARY_DIR}/${TEST_DIR}/last/in.idf" )
-#  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${SOURCE_DIR}/testfiles/BriansFiles/${EPW_FILE}" "${BINARY_DIR}/${TEST_DIR}/last/in.epw" )
-#  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${BINARY_DIR}/Products/Energy+.idd" "${BINARY_DIR}/${TEST_DIR}/last/Energy+.idd" )
-#  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${BINARY_DIR}/Products/ReadVarsESO.exe" "${BINARY_DIR}/${TEST_DIR}/last/ReadVarsESO.exe" )
+  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${SOURCE_DIR}/testfiles/BriansFiles/${IDF_FILE}" "${BINARY_DIR}/${TEST_DIR}/last/in.idf" )
+  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${SOURCE_DIR}/testfiles/BriansFiles/${EPW_FILE}" "${BINARY_DIR}/${TEST_DIR}/last/in.epw" )
+  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${BINARY_DIR}/Products/Energy+.idd" "${BINARY_DIR}/${TEST_DIR}/last/Energy+.idd" )
+  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${BINARY_DIR}/Products/ReadVarsESO.exe" "${BINARY_DIR}/${TEST_DIR}/last/ReadVarsESO.exe" )
   
   # Find and execute the test executable, passing the argument of the directory to run in
-  if( WIN32 )
-    set(ECHO_CMD cmd /C echo.)
-  else()
-    set(ECHO_CMD "echo")
-  endif()
+#  if( WIN32 )
+#    set(ECHO_CMD cmd /C echo.)
+#  else()
+#    set(ECHO_CMD "echo")
+#  endif()
 #  find_program(TEST_EXE TestResetStates PATHS "${BINARY_DIR}/Products/" "${BINARY_DIR}/Products/Release/" "${BINARY_DIR}/Products/Debug/" )
-#  message( "Executing TestResetStates from ${TEST_EXE}" )
-#  message( "Passing run directory as ${TEST_DIR}/last" )
-#  execute_process(COMMAND ${ECHO_CMD} COMMAND "${TEST_EXE}" "${BINARY_DIR}/${TEST_DIR}/last")
-#  execute_process(COMMAND ${ECHO_CMD} COMMAND "${BINARY_DIR}/${TEST_DIR}/last/ReadVarsESO.exe" WORKING_DIRECTORY "${BINARY_DIR}/${TEST_DIR}/last")
+  message( "Executing TestResetStates from ${TEST_EXE}" )
+  message( "Passing run directory as ${TEST_DIR}/last" )
+  execute_process(COMMAND ${ECHO_CMD} COMMAND "${TEST_EXE}" "${BINARY_DIR}/${TEST_DIR}/last")
+  execute_process(COMMAND ${ECHO_CMD} COMMAND "${BINARY_DIR}/${TEST_DIR}/last/ReadVarsESO.exe" WORKING_DIRECTORY "${BINARY_DIR}/${TEST_DIR}/last")
 
   
   #run python
-#  message("${CMAKE_COMMAND} -E remove ${BINARY_DIR}/${TEST_DIR}/out1 ${BINARY_DIR}/${TEST_DIR}/out2 ${BINARY_DIR}/${TEST_DIR}/out3 ${BINARY_DIR}/${TEST_DIR}/out4")
+  message("remove any out1's directories from previous runs")
   
   execute_process(COMMAND "${CMAKE_COMMAND}" -E remove "${BINARY_DIR}/${TEST_DIR}/out1" "${BINARY_DIR}/${TEST_DIR}/out2" "${BINARY_DIR}/${TEST_DIR}/out3" "${BINARY_DIR}/${TEST_DIR}/out4")
   find_package(PythonInterp 2.7 REQUIRED)
@@ -76,16 +76,22 @@
   SET(PYTHON_ARGS ${BINARY_DIR}/${TEST_DIR}/math_diff.py ${BINARY_DIR}\\${TEST_DIR}\\last\\eplusout.csv ${BINARY_DIR}\\${TEST_DIR}\\first\\eplusout.csv ${BINARY_DIR}/${TEST_DIR}/out1 ${BINARY_DIR}/${TEST_DIR}/out2 ${BINARY_DIR}/${TEST_DIR}/out3 ${BINARY_DIR}/${TEST_DIR}/out4)
   message("${PYTHON_EXECUTABLE} ${PYTHON_ARGS}")
   execute_process(COMMAND ${PYTHON_EXECUTABLE} ${PYTHON_ARGS} WORKING_DIRECTORY "${BINARY_DIR}/${TEST_DIR}")
-
-  #execute_process(COMMAND \"${PYTHON_EXECUTABLE}\" \"${BINARY_DIR}/${TEST_DIR}/math_diff.py\" \"${BINARY_DIR}/${TEST_DIR}/last/eplusout.csv\" \"${BINARY_DIR}/${TEST_DIR}/first/eplusout.csv\")
   
   # Check the outputs and return appropriately
   file(READ "${BINARY_DIR}/${TEST_DIR}/out4" FILE_CONTENT)
   message("file content")
   message("${FILE_CONTENT}")
   string(FIND "${FILE_CONTENT}" "All Equal" RESULT)
+  string(FIND "${FILE_CONTENT}" "Small Diffs" RESULT2)
+  #check if string was found (-1 means it wasnt found)
+  #look for All Equal
   if( NOT (RESULT EQUAL -1 ))
     message("Test Passed")
   else()
-    message("Test Failed")
+    #look for Small Diffs
+    if (NOT (RESULT2 EQUAL -1 ))
+      message("Test Passed")
+    else()
+      message("Test Failed")
+    endif()
   endif()
