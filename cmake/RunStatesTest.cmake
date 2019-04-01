78b1d70d84bb59f3ee1f53b2717a37a2cc1bf86a
Brian Ball <Brian.Ball@nrel.gov>
2019-04-01 10:49:09 -0600

    cleanup test file

--- cmake/RunStatesTest.cmake
+++ cmake/RunStatesTest.cmake
@@ -1,74 +1,67 @@
   # Set up for convenience
   set(TEST_DIR "tst/reset_states")
   #set( ENV{DDONLY} y)
-  execute_process(COMMAND "${CMAKE_COMMAND}" -E remove_directory "${BINARY_DIR}/${TEST_DIR}/first")
+  execute_process(COMMAND "${CMAKE_COMMAND}" -E remove_directory "${BINARY_DIR}/${TEST_DIR}/split")
   execute_process(COMMAND "${CMAKE_COMMAND}" -E remove_directory "${BINARY_DIR}/${TEST_DIR}/full")
 
   # Run Full Year
-	  set(SimulationState "!comment")
-	  configure_file("${SOURCE_DIR}/testfiles/BriansFiles/${IDF_FILE}" "${BINARY_DIR}/${TEST_DIR}/full/in.idf")
-	  #execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${SOURCE_DIR}/testfiles/BriansFiles/${IDF_FILE}" "${BINARY_DIR}/${TEST_DIR}/full/in.idf" )
-	  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${SOURCE_DIR}/testfiles/BriansFiles/${EPW_FILE}" "${BINARY_DIR}/${TEST_DIR}/full/in.epw" )
-	  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${BINARY_DIR}/Products/Energy+.idd" "${BINARY_DIR}/${TEST_DIR}/full/Energy+.idd" )
-	  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${BINARY_DIR}/Products/ReadVarsESO.exe" "${BINARY_DIR}/${TEST_DIR}/full/ReadVarsESO.exe" )
+  #copy over idf and use !comment to replace ${SimulationState} var in idf with dummy comment so it still runs full year in /full dir
+  set(SimulationState "!comment")
+  configure_file("${SOURCE_DIR}/testfiles/BriansFiles/${IDF_FILE}" "${BINARY_DIR}/${TEST_DIR}/full/in.idf")
+  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${SOURCE_DIR}/testfiles/BriansFiles/${EPW_FILE}" "${BINARY_DIR}/${TEST_DIR}/full/in.epw" )
+  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${BINARY_DIR}/Products/Energy+.idd" "${BINARY_DIR}/${TEST_DIR}/full/Energy+.idd" )
+  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${BINARY_DIR}/Products/ReadVarsESO.exe" "${BINARY_DIR}/${TEST_DIR}/full/ReadVarsESO.exe" )
 
-	  # Find and execute the test executable, passing the argument of the directory to run in
-	  if( WIN32 )
-		set(ECHO_CMD cmd /C echo.)
-	  else()
-		set(ECHO_CMD "echo")
-	  endif()
-	  find_program(TEST_EXE TestResetStates PATHS "${BINARY_DIR}/Products/" "${BINARY_DIR}/Products/Release/" "${BINARY_DIR}/Products/Debug/" )
-	  message( "Executing TestResetStates from ${TEST_EXE}" )
-	  message( "Passing run directory as ${TEST_DIR}/full" )
-	  execute_process(COMMAND ${ECHO_CMD} COMMAND "${TEST_EXE}" "${BINARY_DIR}/${TEST_DIR}/full")
-	  execute_process(COMMAND ${ECHO_CMD} COMMAND "${BINARY_DIR}/${TEST_DIR}/full/ReadVarsESO.exe" WORKING_DIRECTORY "${BINARY_DIR}/${TEST_DIR}/full")
+  # Find and execute the test executable, passing the argument of the directory to run in
+  if( WIN32 )
+  set(ECHO_CMD cmd /C echo.)
+  else()
+  set(ECHO_CMD "echo")
+  endif()
+  find_program(TEST_EXE TestResetStates PATHS "${BINARY_DIR}/Products/" "${BINARY_DIR}/Products/Release/" "${BINARY_DIR}/Products/Debug/" )
+  #run model and create csv
+  message( "Executing TestResetStates from ${TEST_EXE}" )
+  message( "Passing run directory as ${TEST_DIR}/full" )
+  execute_process(COMMAND ${ECHO_CMD} COMMAND "${TEST_EXE}" "${BINARY_DIR}/${TEST_DIR}/full")
+  execute_process(COMMAND ${ECHO_CMD} COMMAND "${BINARY_DIR}/${TEST_DIR}/full/ReadVarsESO.exe" WORKING_DIRECTORY "${BINARY_DIR}/${TEST_DIR}/full")
 
-  # Clean up
-  #execute_process(COMMAND "${CMAKE_COMMAND}" -E remove "${BINARY_DIR}/${TEST_DIR}/full/Energy+.idd" "${BINARY_DIR}/${TEST_DIR}/full/in.epw")
-
-  # Run First half of Year
-  #TODO modify IDF to do that
-	  set(SimulationState "SimulationStates,RUNPERIOD 1,save,stop,182,24,1;")
- 	  #set(SimulationState "SimulationStates,RUNPERIOD 1,save,continue,31,24,1;")
-	  configure_file("${SOURCE_DIR}/testfiles/BriansFiles/${IDF_FILE}" "${BINARY_DIR}/${TEST_DIR}/first/in.idf")
-	  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${SOURCE_DIR}/testfiles/BriansFiles/${EPW_FILE}" "${BINARY_DIR}/${TEST_DIR}/first/in.epw" )
-	  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${BINARY_DIR}/Products/Energy+.idd" "${BINARY_DIR}/${TEST_DIR}/first/Energy+.idd" )
-	  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${BINARY_DIR}/Products/ReadVarsESO.exe" "${BINARY_DIR}/${TEST_DIR}/first/ReadVarsESO.exe" )
-	  
-	  message( "Executing TestResetStates from ${TEST_EXE}" )
-	  message( "Passing run directory as ${TEST_DIR}/first" )
-	  execute_process(COMMAND ${ECHO_CMD} COMMAND "${TEST_EXE}" "${BINARY_DIR}/${TEST_DIR}/first")
-	  execute_process(COMMAND ${ECHO_CMD} COMMAND "${BINARY_DIR}/${TEST_DIR}/first/ReadVarsESO.exe" WORKING_DIRECTORY "${BINARY_DIR}/${TEST_DIR}/first")
-	  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${BINARY_DIR}/${TEST_DIR}/first/eplusout.csv" "${BINARY_DIR}/${TEST_DIR}/first/eplusout_first.csv" )
-
-  # Clean up remove idf
-  execute_process(COMMAND "${CMAKE_COMMAND}" -E remove "${BINARY_DIR}/${TEST_DIR}/first/in.idf")
-
-  # Run Last half of Year
-  #TODO modify IDF to do that
-
-  SET(SimulationState "SimulationStates,RUNPERIOD 1,load,continue,182,24,1;")
-  configure_file("${SOURCE_DIR}/testfiles/BriansFiles/${IDF_FILE}" "${BINARY_DIR}/${TEST_DIR}/first/in.idf")
-  #execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${SOURCE_DIR}/testfiles/BriansFiles/${EPW_FILE}" "${BINARY_DIR}/${TEST_DIR}/first/in.epw" )
-  #execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${BINARY_DIR}/Products/Energy+.idd" "${BINARY_DIR}/${TEST_DIR}/first/Energy+.idd" )
-  #execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${BINARY_DIR}/Products/ReadVarsESO.exe" "${BINARY_DIR}/${TEST_DIR}/first/ReadVarsESO.exe" )
+  # Run First half of Year in /split dir
+  set(SimulationState "SimulationStates,RUNPERIOD 1,save,stop,182,24,1;")
+  configure_file("${SOURCE_DIR}/testfiles/BriansFiles/${IDF_FILE}" "${BINARY_DIR}/${TEST_DIR}/split/in.idf")
+  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${SOURCE_DIR}/testfiles/BriansFiles/${EPW_FILE}" "${BINARY_DIR}/${TEST_DIR}/split/in.epw" )
+  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${BINARY_DIR}/Products/Energy+.idd" "${BINARY_DIR}/${TEST_DIR}/split/Energy+.idd" )
+  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${BINARY_DIR}/Products/ReadVarsESO.exe" "${BINARY_DIR}/${TEST_DIR}/split/ReadVarsESO.exe" )
   
   message( "Executing TestResetStates from ${TEST_EXE}" )
-  message( "Passing run directory as ${TEST_DIR}/first" )
-  execute_process(COMMAND ${ECHO_CMD} COMMAND "${TEST_EXE}" "${BINARY_DIR}/${TEST_DIR}/first")
-  execute_process(COMMAND ${ECHO_CMD} COMMAND "${BINARY_DIR}/${TEST_DIR}/first/ReadVarsESO.exe" WORKING_DIRECTORY "${BINARY_DIR}/${TEST_DIR}/first")
+  message( "Passing run directory as ${TEST_DIR}/split" )
+  execute_process(COMMAND ${ECHO_CMD} COMMAND "${TEST_EXE}" "${BINARY_DIR}/${TEST_DIR}/split")
+  execute_process(COMMAND ${ECHO_CMD} COMMAND "${BINARY_DIR}/${TEST_DIR}/split/ReadVarsESO.exe" WORKING_DIRECTORY "${BINARY_DIR}/${TEST_DIR}/split")
+  #make copy of csv for debugging
+  execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${BINARY_DIR}/${TEST_DIR}/split/eplusout.csv" "${BINARY_DIR}/${TEST_DIR}/split/eplusout_first.csv" )
 
+  # Run Last half of Year
+  # remove idf and then copy over new idf with load state functionality
+  execute_process(COMMAND "${CMAKE_COMMAND}" -E remove "${BINARY_DIR}/${TEST_DIR}/split/in.idf")
+
+  SET(SimulationState "SimulationStates,RUNPERIOD 1,load,continue,182,24,1;")
+  configure_file("${SOURCE_DIR}/testfiles/BriansFiles/${IDF_FILE}" "${BINARY_DIR}/${TEST_DIR}/split/in.idf")
   
-  #run python
+  message( "Executing TestResetStates from ${TEST_EXE}" )
+  message( "Passing run directory as ${TEST_DIR}/split" )
+  execute_process(COMMAND ${ECHO_CMD} COMMAND "${TEST_EXE}" "${BINARY_DIR}/${TEST_DIR}/split")
+  execute_process(COMMAND ${ECHO_CMD} COMMAND "${BINARY_DIR}/${TEST_DIR}/split/ReadVarsESO.exe" WORKING_DIRECTORY "${BINARY_DIR}/${TEST_DIR}/split")
+  
+  # run python
+  # cmd is: python mathdiff <input1> <input2> <abs_diff_out> <rel_diff_out> <err_log> <summary_csv>
+  # we use out1 out2 out3 out4 as the out files 
+  # delete them if they already exist before the run
   message("remove any out1's directories from previous runs")
-  
   execute_process(COMMAND "${CMAKE_COMMAND}" -E remove "${BINARY_DIR}/${TEST_DIR}/out1" "${BINARY_DIR}/${TEST_DIR}/out2" "${BINARY_DIR}/${TEST_DIR}/out3" "${BINARY_DIR}/${TEST_DIR}/out4")
   
   find_package(PythonInterp 2.7 REQUIRED)
   
   message("Trying Python Call")
-  set(PYTHON_ARGS ${BINARY_DIR}/${TEST_DIR}/math_diff.py ${BINARY_DIR}\\${TEST_DIR}\\full\\eplusout.csv ${BINARY_DIR}\\${TEST_DIR}\\first\\eplusout.csv ${BINARY_DIR}/${TEST_DIR}/out1 ${BINARY_DIR}/${TEST_DIR}/out2 ${BINARY_DIR}/${TEST_DIR}/out3 ${BINARY_DIR}/${TEST_DIR}/out4)
+  set(PYTHON_ARGS ${BINARY_DIR}/${TEST_DIR}/math_diff.py ${BINARY_DIR}\\${TEST_DIR}\\full\\eplusout.csv ${BINARY_DIR}\\${TEST_DIR}\\split\\eplusout.csv ${BINARY_DIR}/${TEST_DIR}/out1 ${BINARY_DIR}/${TEST_DIR}/out2 ${BINARY_DIR}/${TEST_DIR}/out3 ${BINARY_DIR}/${TEST_DIR}/out4)
   message("${PYTHON_EXECUTABLE} ${PYTHON_ARGS}")
   execute_process(COMMAND ${PYTHON_EXECUTABLE} ${PYTHON_ARGS} WORKING_DIRECTORY "${BINARY_DIR}/${TEST_DIR}")
   
